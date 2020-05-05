bfdba5312ccac0ea887ce1ad42d0b96888f4a16b
Syuparn <t.another.add@gmail.com>
2020-05-05 18:56:52 +0900

    imple PanRange

--- object/range.go
+++ object/range.go
@@ -1,6 +1,9 @@
 package object
 
-import ()
+import (
+	"bytes"
+	"strings"
+)
 
 const RANGE_TYPE = "RANGE_TYPE"
 
@@ -11,13 +14,22 @@ type PanRange struct {
 }
 
 func (r *PanRange) Type() PanObjType {
-	return ""
+	return RANGE_TYPE
 }
 
 func (r *PanRange) Inspect() string {
-	return ""
+	var out bytes.Buffer
+	elems := make([]string, 3)
+	elems[0] = r.Start.Inspect()
+	elems[1] = r.Stop.Inspect()
+	elems[2] = r.Step.Inspect()
+
+	out.WriteString("(")
+	out.WriteString(strings.Join(elems, ":"))
+	out.WriteString(")")
+	return out.String()
 }
 
 func (r *PanRange) Proto() PanObject {
-	return r
+	return builtInRangeObj
 }
