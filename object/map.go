217432ee1b5b87c221cb88786340c3371a80271d
Syuparn <t.another.add@gmail.com>
2020-05-06 13:50:20 +0900

    impl PanMap

--- object/map.go
+++ object/map.go
@@ -1,5 +1,9 @@
 package object
 
+import (
+	"bytes"
+)
+
 const MAP_TYPE = "MAP_TYPE"
 
 type PanMap struct {
@@ -7,13 +11,27 @@ type PanMap struct {
 }
 
 func (m *PanMap) Type() PanObjType {
-	return ""
+	return MAP_TYPE
 }
 
 func (m *PanMap) Inspect() string {
-	return ""
+	var out bytes.Buffer
+	pairs := []Pair{}
+
+	// NOTE: refer map because range cannot treat map pointer
+	for _, p := range *m.Pairs {
+		pairs = append(pairs, p)
+	}
+
+	out.WriteString("%{")
+	// NOTE: sort by key order otherwise output changes randomly
+	// depending on inner map structure
+	out.WriteString(sortedPairsString(pairs))
+	out.WriteString("}")
+
+	return out.String()
 }
 
 func (m *PanMap) Proto() PanObject {
-	return m
+	return BuiltInMapObj
 }
