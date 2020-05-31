f6c2632bf7e4f04cf86273530d873a42486c0d1b
Syuparn <t.another.add@gmail.com>
2020-05-31 16:47:36 +0900

    eval range literal

---
+++ evaluator/eval_range.go
@@ -0,0 +1,22 @@
+package evaluator
+
+import (
+	"../ast"
+	"../object"
+)
+
+func evalRange(node *ast.RangeLiteral, env *object.Env) object.PanObject {
+	// NOTE: *ast.RangeLiteral has nil (not NilLiteral) if nothing is set
+	return &object.PanRange{
+		Start: evalOrNil(node.Start, env),
+		Stop:  evalOrNil(node.Stop, env),
+		Step:  evalOrNil(node.Step, env),
+	}
+}
+
+func evalOrNil(node ast.Node, env *object.Env) object.PanObject {
+	if node == nil {
+		return object.BuiltInNil
+	}
+	return Eval(node, env)
+}
