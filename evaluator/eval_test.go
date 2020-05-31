b51f897ba1a507a2c7d0b1dd899f154f3f286129
Syuparn <t.another.add@gmail.com>
2020-05-31 14:24:24 +0900

    test: eval float literal

--- evaluator/eval_test.go
+++ evaluator/eval_test.go
@@ -46,6 +46,32 @@ func TestEvalIntLiteral(t *testing.T) {
 	}
 }
 
+func TestEvalFloatLiteral(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected float64
+	}{
+		{"5.0", 5.0},
+		{"12.345", 12.345},
+		{"-1.4", -1.4},
+		// special forms
+		{"1_0.2", 10.2},
+		{"1.2e1", 12.0},
+		{"1.2e-1", 0.12},
+		{".25", 0.25},
+		{"-1_0.2", -10.2},
+		{"-1.2e1", -12.0},
+		{"-1.2e-1", -0.12},
+		{"-.25", -0.25},
+	}
+
+	for _, tt := range tests {
+		actual := testEval(t, tt.input)
+		expected := &object.PanFloat{Value: tt.expected}
+		testPanFloat(t, actual, expected)
+	}
+}
+
 func testPanInt(t *testing.T, actual object.PanObject, expected *object.PanInt) {
 	if actual.Type() != object.INT_TYPE {
 		t.Fatalf("Type must be INT_TYPE. got=%s", actual.Type())
@@ -63,9 +89,30 @@ func testPanInt(t *testing.T, actual object.PanObject, expected *object.PanInt) 
 	}
 }
 
+func testPanFloat(t *testing.T, actual object.PanObject, expected *object.PanFloat) {
+	if actual.Type() != object.FLOAT_TYPE {
+		t.Fatalf("Type must be FLOAT_TYPE. got=%s", actual.Type())
+		return
+	}
+
+	floatObj, ok := actual.(*object.PanFloat)
+	if !ok {
+		t.Fatalf("actual must be *object.PanFloat. got=%T (%v)", actual, actual)
+		return
+	}
+
+	if floatObj.Value != expected.Value {
+		t.Errorf("wrong value. expected=%f, got=%f", expected.Value, floatObj.Value)
+	}
+}
+
 func testEval(t *testing.T, input string) object.PanObject {
 	node := testParse(t, input)
-	return Eval(node, object.NewEnv())
+	panObject := Eval(node, object.NewEnv())
+	if panObject == nil {
+		t.Fatalf("Eval() returned nothing (input=`%s`)", input)
+	}
+	return panObject
 }
 
 func testParse(t *testing.T, input string) *ast.Program {
