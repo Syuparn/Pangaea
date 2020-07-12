e2fd64b920eb56f983b576d79f6fba6b4c1b9c90
Syuparn <t.another.add@gmail.com>
2020-07-12 15:41:03 +0900

    enable to eval iter

---
+++ evaluator/eval_iter.go
@@ -0,0 +1,10 @@
+package evaluator
+
+import (
+	"../ast"
+	"../object"
+)
+
+func evalIter(node *ast.IterLiteral, env *object.Env) object.PanObject {
+	return evalCallable(node.FuncComponent, env, object.ITER_FUNC)
+}
