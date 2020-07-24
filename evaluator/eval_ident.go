88c1c0796a55c5d68cfe96fac9dc893ad324a4e3
Syuparn <t.another.add@gmail.com>
2020-07-24 11:30:05 +0900

    add consts

--- evaluator/eval_ident.go
+++ evaluator/eval_ident.go
@@ -8,13 +8,10 @@ import (
 
 func evalIdent(ident *ast.Ident, env *object.Env) object.PanObject {
 	// check if ident refers keyword
-	switch ident.Value {
-	case "true":
-		return object.BuiltInTrue
-	case "false":
-		return object.BuiltInFalse
-	case "nil":
-		return object.BuiltInNil
+	const_, ok := constVal(ident.Value)
+
+	if ok {
+		return const_
 	}
 
 	val, ok := env.Get(object.GetSymHash(ident.Value))
@@ -26,4 +23,61 @@ func evalIdent(ident *ast.Ident, env *object.Env) object.PanObject {
 	}
 
 	return val
+}
+
+func constVal(name string) (object.PanObject, bool) {
+	switch name {
+	case "Int":
+		return object.BuiltInIntObj, true
+	case "Float":
+		return object.BuiltInFloatObj, true
+	case "Num":
+		return object.BuiltInNumObj, true
+	case "Nil":
+		return object.BuiltInNilObj, true
+	case "Str":
+		return object.BuiltInStrObj, true
+	case "Arr":
+		return object.BuiltInArrObj, true
+	case "Range":
+		return object.BuiltInRangeObj, true
+	case "Func":
+		return object.BuiltInFuncObj, true
+	case "Match":
+		return object.BuiltInMatchObj, true
+	case "Obj":
+		return object.BuiltInObjObj, true
+	case "BaseObj":
+		return object.BuiltInBaseObj, true
+	case "Map":
+		return object.BuiltInMapObj, true
+	case "true":
+		return object.BuiltInTrue, true
+	case "false":
+		return object.BuiltInFalse, true
+	case "nil":
+		return object.BuiltInNil, true
+	case "Err":
+		return object.BuiltInErrObj, true
+	case "AssertionErr":
+		return object.BuiltInAssertionErr, true
+	case "NameErr":
+		return object.BuiltInNameErr, true
+	case "NoPropErr":
+		return object.BuiltInNoPropErr, true
+	case "NotImplementedErr":
+		return object.BuiltInNotImplementedErr, true
+	case "SyntaxErr":
+		return object.BuiltInSyntaxErr, true
+	case "TypeErr":
+		return object.BuiltInTypeErr, true
+	case "ValueErr":
+		return object.BuiltInValueErr, true
+	case "ZeroDivisionErr":
+		return object.BuiltInZeroDivisionErr, true
+	case "_":
+		return object.NewNotImplementedErr("Not implemented"), true
+	default:
+		return nil, false
+	}
 }
