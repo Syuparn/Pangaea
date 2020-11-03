87f93db255f1cd5fb7030368cba3d9693979e2d3
Syuparn <t.another.add@gmail.com>
2020-11-03 20:04:24 +0900

    main: add test mode

--- runscript/run.go
+++ runscript/run.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/Syuparn/pangaea/di"
 	"github.com/Syuparn/pangaea/evaluator"
@@ -13,17 +15,35 @@ import (
 
 // Run runs Pangaea script source file.
 func Run(fileName string, in io.Reader, out io.Writer) int {
-	env := object.NewEnvWithConsts()
-	// setup object `IO`
-	env.InjectIO(in, out)
+	env := setup(in, out)
+	exitCode := run(fileName, in, out, env)
+	return exitCode
+}
 
-	// necessary to setup built-in object props
-	di.InjectBuiltInProps(env)
+// RunTest runs all test script files in path until error is raised.
+func RunTest(path string, in io.Reader, out io.Writer) int {
+	env := setup(in, out)
+	exitCode := 0
 
-	// enable to use Kernel props directly in top-level
-	// NOTE: InjectFrom must be called after BuiltInKernelObj is set up
-	env.InjectFrom(object.BuiltInKernelObj)
+	filepath.Walk(path, func(path string, info os.FileInfo, err error) error {
+		if !strings.HasSuffix(path, ".pangaea") {
+			return nil
+		}
 
+		out.Write([]byte(fmt.Sprintf("run:  %s\n", path)))
+
+		exitCode = run(path, in, out, env)
+		if exitCode != 0 {
+			return fmt.Errorf("test in %s failed", path)
+		}
+
+		out.Write([]byte(fmt.Sprintf("pass: %s\n", path)))
+		return nil
+	})
+	return exitCode
+}
+
+func run(fileName string, in io.Reader, out io.Writer, env *object.Env) int {
 	fp, err := os.Open(fileName)
 	if err != nil {
 		fmt.Fprint(os.Stderr, err.Error()+"\n")
@@ -45,4 +65,19 @@ func Run(fileName string, in io.Reader, out io.Writer) int {
 	}
 
 	return 0
+}
+
+func setup(in io.Reader, out io.Writer) *object.Env {
+	env := object.NewEnvWithConsts()
+	// setup object `IO`
+	env.InjectIO(in, out)
+
+	// necessary to setup built-in object props
+	di.InjectBuiltInProps(env)
+
+	// enable to use Kernel props directly in top-level
+	// NOTE: InjectFrom must be called after BuiltInKernelObj is set up
+	env.InjectFrom(object.BuiltInKernelObj)
+
+	return env
 }
