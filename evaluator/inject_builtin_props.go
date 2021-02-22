52cb3e89eb5081848947f7438f2e065ca0e2c7d3
Syuparn <t.another.add@gmail.com>
2021-02-22 20:58:24 +0900

    evaluator: use NewPanBuiltInFunc()

--- evaluator/inject_builtin_props.go
+++ evaluator/inject_builtin_props.go
@@ -9,14 +9,14 @@ import (
 func NewPropContainer() map[string]object.PanObject {
 	return map[string]object.PanObject{
 		// name format: "ObjName_propName"
-		"Arr_at":       &object.PanBuiltIn{Fn: findElemInArr},
-		"BaseObj_at":   &object.PanBuiltIn{Fn: findElemInObj},
-		"Func_call":    &object.PanBuiltIn{Fn: evalFuncCall},
-		"Int_at":       &object.PanBuiltIn{Fn: findBitInInt},
-		"Iter_new":     &object.PanBuiltIn{Fn: iterNew},
-		"Iter_next":    &object.PanBuiltIn{Fn: iterNext},
-		"Map_at":       &object.PanBuiltIn{Fn: findElemInMap},
-		"Obj_callProp": &object.PanBuiltIn{Fn: builtInCallProp},
-		"Str_at":       &object.PanBuiltIn{Fn: findElemInStr},
+		"Arr_at":       object.NewPanBuiltInFunc(findElemInArr),
+		"BaseObj_at":   object.NewPanBuiltInFunc(findElemInObj),
+		"Func_call":    object.NewPanBuiltInFunc(evalFuncCall),
+		"Int_at":       object.NewPanBuiltInFunc(findBitInInt),
+		"Iter_new":     object.NewPanBuiltInFunc(iterNew),
+		"Iter_next":    object.NewPanBuiltInFunc(iterNext),
+		"Map_at":       object.NewPanBuiltInFunc(findElemInMap),
+		"Obj_callProp": object.NewPanBuiltInFunc(builtInCallProp),
+		"Str_at":       object.NewPanBuiltInFunc(findElemInStr),
 	}
 }
