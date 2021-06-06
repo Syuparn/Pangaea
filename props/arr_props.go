1067bfbe2029c0f0cadb243e7c091edc56f8b41c
Syuparn <t.another.add@gmail.com>
2021-06-06 17:40:48 +0900

    props: add Arr#M

--- props/arr_props.go
+++ props/arr_props.go
@@ -220,6 +220,42 @@ func ArrProps(propContainer map[string]object.PanObject) map[string]object.PanOb
 				return object.NewPanInt(int64(len(self.Elems)))
 			},
 		),
+		"M": f(
+			func(
+				env *object.Env, kwargs *object.PanObj, args ...object.PanObject,
+			) object.PanObject {
+				if len(args) < 1 {
+					return object.NewTypeErr("Arr#M requires at least 1 arg")
+				}
+				self, ok := object.TraceProtoOfArr(args[0])
+				if !ok {
+					return object.NewTypeErr(
+						fmt.Sprintf("%s cannot be treated as arr", object.ReprStr(args[0])))
+				}
+
+				pairs := make([]object.Pair, len(self.Elems))
+
+				for i, e := range self.Elems {
+					arr, ok := object.TraceProtoOfArr(e)
+					if !ok {
+						return object.NewValueErr(
+							fmt.Sprintf(`element %s cannot be treated as arr`, object.ReprStr(e)))
+					}
+
+					if len(arr.Elems) != 2 {
+						return object.NewValueErr(
+							fmt.Sprintf(`element %s must have two elements`, object.ReprStr(arr)))
+					}
+
+					pairs[i] = object.Pair{
+						Key:   arr.Elems[0],
+						Value: arr.Elems[1],
+					}
+				}
+
+				return object.NewPanMap(pairs...)
+			},
+		),
 		"new": f(
 			func(
 				env *object.Env, kwargs *object.PanObj, args ...object.PanObject,
